73ad4caae79134d3b46baf7b87c80c52f2fe6a8b
daniele <zeolla.d@gmail.com>
2021-11-12 17:59:22 +0100

    Agginto codice creazione socket e per accettare nuove connessioni

--- src/server/server.c
+++ src/server/server.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 
 #include <config.h>
 #include <connectionQueue.h>
 
 static Config* serverConfig;
+static ConnectionQueue *queue;
 
 static void* worker();
 
 int main(int argc, char *argv[]){
 
-    /****** CONFIG SECTION *******/
+    /****** CONFIG INIT *******/
     serverConfig = malloc(sizeof(Config));
     int res = readConfig(serverConfig);
     if(res == -1){
@@ -21,16 +24,54 @@ int main(int argc, char *argv[]){
     }
     printConfig(serverConfig);
 
-    /******* THREAD SECTION *****/
-    pthread_t *threadPool;
-    threadPool =(pthread_t*)malloc(serverConfig->thread_workers * sizeof(pthread_t) ); // Alloco un array per i descrittori di processo
+    /***** CONNECTION QUEUE INIT *****/
+    queue = initQueue();
 
-    for(int i = 0; i<serverConfig->thread_workers; i++) {
-        pthread_create(&threadPool[i], NULL, &worker, NULL);
+    /******* THREAD INIT *****/
+    pthread_t *threadPool;
+    int *threadPoolStatus;
+    // Alloco un array per i descrittori di processo
+    threadPool = (pthread_t*)malloc(serverConfig->thread_workers * sizeof(pthread_t) );
+    threadPoolStatus = (int*) malloc(serverConfig->thread_workers * sizeof(int));
+
+//    for(int i = 0; i<serverConfig->thread_workers; i++) {
+//        pthread_create(&threadPool[i], NULL, &worker, NULL);
+//    }
+
+//    for(int i = 0; i<serverConfig->thread_workers; i++) {
+//        pthread_join(threadPool[i], (void *) &threadPoolStatus[i]);
+//    }
+
+    /***** SOCKET INIT *****/
+    int fd_server_skt;
+    int fd_client_skt; //File descriptor per connessioni client
+    struct sockaddr_un socketAddress;
+
+    strncpy(socketAddress.sun_path, serverConfig->socket_path,100);
+    socketAddress.sun_family = AF_UNIX; // Socket di tipo AF_UNIX
+
+    //Creazione socket
+    if ( (fd_server_skt = socket(AF_UNIX, SOCK_STREAM, 0)) == -1 ){
+        printf("Errore creazione socket, errno: %d\n",errno);
+        exit(-1);
     }
-    int status;
-    for(int i = 0; i<serverConfig->thread_workers; i++) {
-        pthread_join(threadPool[i], (void *) &status);
+    //Bind socket con address
+    if ( (bind(fd_server_skt, (struct sockaddr*) &socketAddress, sizeof(socketAddress)) ) == -1 ){
+        printf("Errore bind socket, errno: %d\n",errno);
+        exit(-1);
+    }
+
+    if ( listen(fd_server_skt,SOMAXCONN) == -1){
+        printf("Errore listen socket, errno: %d\n",errno);
+        exit(-1);
+    }
+    /****** MAIN SERVER LOOP *****/
+    printf("\n***** ATTENDO NUOVE CONNESSIONI *****\n");
+
+    if ( (fd_client_skt = accept(fd_server_skt,NULL,0)) == -1 ){
+        printf("Errore accept socket, errno: %d\n",errno);
+    }else{
+        printf("NUOVA CONNESSIONE RICEVUTA!\n");
     }
 
     return 0;
