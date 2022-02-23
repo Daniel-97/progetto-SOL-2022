18964dc06c60ea6603d51e6812f1e70d7355f2d6
daniele <zeolla.d@gmail.com>
2022-02-23 22:32:38 +0100

    Aggiunti file di test

--- script/test1.sh
+++ script/test1.sh
@@ -1,11 +1,18 @@
 CONFIG=./config/config1.txt
-SERVERG=./bin/server
+SERVER=./bin/server
 CLIENT=./bin/client
+SOCKET=./connection.sk
 
 N_WORKER=1
 N_FILE=10000
 STORAGE_SIZE=128000000
 
 #Scrivo le configurazioni nel file
-echo -e "socket-path:./connection.sk\nthread-workers:${N_WORKER}\nmax-mem-size:${STORAGE_SIZE}\nmax-file:${N_FILE}\nlog-file:./log.txt" > ${CONFIG}
+echo -e "socket-path:${SOCKET}\nthread-workers:${N_WORKER}\nmax-mem-size:${STORAGE_SIZE}\nmax-file:${N_FILE}\nlog-file:./log.txt" > ${CONFIG}
 
+#lancio server
+valgrind --leak-check=full ${SERVER} &
+PID_SERVER=$!
+
+#Lancio client 1
+${CLIENT} -f ${SOCKET} -p -t 200
